16080c2f5e1eac1ff2b81d9ae0350040b4c9533f
Angelo Geulin <angelogeulin123@gmail.com>
2023-03-04 20:33:48 +0800

    refactor: remove fragment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,27 +7,25 @@ const inter = Inter({ subsets: ["latin"], variable: "--font-inter" })
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <div className={`${inter.variable} font-sans text-gray-900`}>
-        <div className="min-h-[100svh] grid grid-rows-[5rem_1fr]">
-          <nav className="border-b flex items-center">
-            <div className="max-w-7xl mx-auto w-full flex justify-between items-center">
-              <h1 className="text-2xl font-semibold">Filipino de Cuisine</h1>
-              <div className="flex gap-4">
-                <span>Menu</span>
-                <span>Branches</span>
-                <span>Contact Us</span>
-                <span>About</span>
-              </div>
+    <div className={`${inter.variable} font-sans text-gray-900`}>
+      <div className="min-h-[100svh] grid grid-rows-[5rem_1fr]">
+        <nav className="border-b flex items-center">
+          <div className="max-w-7xl mx-auto w-full flex justify-between items-center">
+            <h1 className="text-2xl font-semibold">Filipino de Cuisine</h1>
+            <div className="flex gap-4">
+              <span>Menu</span>
+              <span>Branches</span>
+              <span>Contact Us</span>
+              <span>About</span>
             </div>
-          </nav>
-          <Component {...pageProps} />
-        </div>
-        <footer className="text-center py-3 bg-gray-700 text-white font-medium text-sm">
-          &copy; 2023 SBIT-3C
-        </footer>
+          </div>
+        </nav>
+        <Component {...pageProps} />
       </div>
-    </>
+      <footer className="text-center py-3 bg-gray-700 text-white font-medium text-sm">
+        &copy; 2023 SBIT-3C
+      </footer>
+    </div>
   )
 }
 
