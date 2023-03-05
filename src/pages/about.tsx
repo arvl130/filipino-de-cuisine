27a5ef1e90539c85a07536f259b5a7cf65f6fa41
Angelo Geulin <angelogeulin123@gmail.com>
2023-03-05 20:33:19 +0800

    feat: add placeholder for about us, contact us, privacy policy page

---
+++ src/pages/about.tsx
@@ -0,0 +1,3 @@
+export default function AboutUsPage() {
+  return <main className="max-w-6xl mx-auto w-full">About Us Page</main>
+}
---
+++ src/pages/privacy.tsx
@@ -0,0 +1,3 @@
+export default function PrivacyPolicyPage() {
+  return <main>Privacy Policy Page</main>
+}
