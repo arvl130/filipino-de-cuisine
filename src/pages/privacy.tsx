f80eb6d6bdd1e730b7ab78f72461945e088d089b
Angelo Geulin <angelogeulin123@gmail.com>
2023-04-03 19:16:09 +0800

    fix: padding for line numbers

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -2,86 +2,127 @@ import Image from "next/image"
 import Link from "next/link"
 
 export default function HeaderSection() {
-  return <section className="privacy">
-    <div className="privacy">
-        <div className="header">
-        <div className="image">
-          <Link href={"/terms-and-condition"}>
-      <Image
-            src="/assets/privacy/symbol2.png"
-            alt="previous"
-            width={40}
-            height={40}
-            
-          />
-
-      </Link>
-      </div>
-      <div className="title">
-            <h4> Legal Terms </h4>
-            <h1> Filipino de Cuisine Privacy Policy </h1>
-            <p>Last Update: March 25, 2023</p>
+  return (
+    <section className="privacy">
+      <div className="privacy">
+        <div className="[background-color:_#10b981] h-72 flex items-center">
+          <div className="max-w-5xl mx-auto grid grid-cols-[4rem_1fr] items-center w-full px-6">
+            <div className="image">
+              <Link href={"/terms-and-condition"}>
+                <Image
+                  src="/assets/privacy/symbol2.png"
+                  alt="next"
+                  width={40}
+                  height={40}
+                />
+              </Link>
             </div>
+
+            <div>
+              <h4 className="text-white font-semibold text-xl mb-2">
+                Legal Terms
+              </h4>
+              <h1 className="text-white font-bold text-3xl mb-2">
+                Filipino de Cuisine Privacy Policy
+              </h1>
+              <p className="text-xl text-white font-medium">
+                Last updated: March 25, 2023
+              </p>
+            </div>
+          </div>
         </div>
 
-        <div className="description">
-            <h6>
-            Welcome to Filipino de Cuisine restaurant&apos;s website. By accessing or using our website, you 
-            agree to be bound by the following terms and conditions:
-            </h6>
-            <p>
-              1.  Use of Website Content: All content on our website, including but not limited to, text, 
-              graphics, images, logos, and software is the property of Filipino de Cuisine restaurant and is 
-              protected by applicable intellectual property laws. You may use the content only for your 
-              personal, non-commercial use, and not for any other purpose without our express written consent.
-              <br></br> <br></br>
-              2. User Conduct: You agree to use our website only for lawful purposes and in a manner that does
-               not infringe upon the rights of others or interfere with the operation of our website. You may 
-               not upload, post, transmit, distribute, or otherwise publish any content that is unlawful, 
-               harmful, threatening, abusive, harassing, defamatory, vulgar, obscene, invasive of another&apos;s 
-               privacy, or otherwise objectionable.
-              <br></br> <br></br>
-              3. Reservation: You may make a reservation for a table at our restaurant through our website. 
-              All reservations are subject to availability and confirmation by Filipino de Cuisine restaurant.
-              <br></br> <br></br>
-              4. Payment: Payment for your meal at our restaurant will be processed in person at the 
+        <div className="description max-w-4xl mx-auto text-justify px-6 my-20">
+          <h6>
+            Welcome to Filipino de Cuisine restaurant&apos;s website. By
+            accessing or using our website, you agree to be bound by the
+            following terms and conditions:
+          </h6>
+
+          <ul className="list-decimal list-outside pl-6 my-6">
+            <li className="mb-6">
+              <span className="font-medium">Use of Website Content</span>: All
+              content on our website, including but not limited to, text,
+              graphics, images, logos, and software is the property of Filipino
+              de Cuisine restaurant and is protected by applicable intellectual
+              property laws. You may use the content only for your personal,
+              non-commercial use, and not for any other purpose without our
+              express written consent.
+            </li>
+            <li className="mb-6">
+              <span className="font-medium">User Conduct</span>: You agree to
+              use our website only for lawful purposes and in a manner that does
+              not infringe upon the rights of others or interfere with the
+              operation of our website. You may not upload, post, transmit,
+              distribute, or otherwise publish any content that is unlawful,
+              harmful, threatening, abusive, harassing, defamatory, vulgar,
+              obscene, invasive of another&apos;s privacy, or otherwise
+              objectionable.
+            </li>
+            <li className="mb-6">
+              <span className="font-medium">Reservation</span>: You may make a
+              reservation for a table at our restaurant through our website. All
+              reservations are subject to availability and confirmation by
+              Filipino de Cuisine restaurant.
+            </li>
+            <li className="mb-6">
+              <span className="font-medium">Payment</span>: Payment for your
+              meal at our restaurant will be processed in person at the
               restaurant. We accept cash and major credit cards.
-              <br></br> <br></br>
-              5. Links to Third-Party Websites: Our website may contain links to third-party websites that are 
-              not owned or controlled by Filipino de Cuisine restaurant. We are not responsible for the content
-               or privacy practices of these websites and do not endorse or make any representations about them.
-                <br></br> <br></br>
-              6. Disclaimer of Warranties: We make no representations or warranties of any kind, express or 
-              implied, regarding the operation of our website or the information, content, materials, or 
-              products included on our website. You use our website at your own risk.
-              <br></br> <br></br>
-              7. Limitation of Liability: We will not be liable for any damages of any kind arising from the 
-              use of our website, including but not limited to direct, indirect, incidental, punitive, and 
-              consequential damages.
-               <br></br> <br></br>
-              8. Indemnification: You agree to indemnify, defend, and hold harmless Filipino de Cuisine 
-              restaurant and its affiliates, officers, directors, employees, agents, and licensors from any 
-              claim, demand, or damage, including reasonable attorneys&apos; fees, arising out of your use of our 
+            </li>
+            <li className="mb-6">
+              <span className="font-medium">Links to Third-Party Websites</span>
+              : Our website may contain links to third-party websites that are
+              not owned or controlled by Filipino de Cuisine restaurant. We are
+              not responsible for the content or privacy practices of these
+              websites and do not endorse or make any representations about
+              them.
+            </li>
+            <li className="mb-6">
+              <span className="font-medium">Disclaimer of Warranties</span>: We
+              make no representations or warranties of any kind, express or
+              implied, regarding the operation of our website or the
+              information, content, materials, or products included on our
+              website. You use our website at your own risk.
+            </li>
+            <li className="mb-6">
+              <span className="font-medium">Limitation of Liability</span>: We
+              will not be liable for any damages of any kind arising from the
+              use of our website, including but not limited to direct, indirect,
+              incidental, punitive, and consequential damages.
+            </li>
+            <li className="mb-6">
+              <span className="font-medium">Indemnification</span>: You agree to
+              indemnify, defend, and hold harmless Filipino de Cuisine
+              restaurant and its affiliates, officers, directors, employees,
+              agents, and licensors from any claim, demand, or damage, including
+              reasonable attorneys&apos; fees, arising out of your use of our
               website or your violation of these terms and conditions.
-              <br></br> <br></br>
-              9. Modification of Terms: We reserve the right to modify these terms and conditions at any 
-              time without prior notice. Your continued use of our website after any such changes constitutes 
-              your acceptance of the new terms and conditions.
-              <br></br> <br></br>
-              10. Governing Law: These terms and conditions are governed by and construed in accordance with 
-              the laws of the Philippines. Any dispute arising out of or related to these terms and conditions 
-              shall be resolved exclusively in the courts of the Philippines.
-            </p>
+            </li>
+            <li className="mb-6">
+              <span className="font-medium">Modification of Terms</span>: We
+              reserve the right to modify these terms and conditions at any time
+              without prior notice. Your continued use of our website after any
+              such changes constitutes your acceptance of the new terms and
+              conditions.
+            </li>
+            <li>
+              <span className="font-medium">Governing Law</span>: These terms
+              and conditions are governed by and construed in accordance with
+              the laws of the Philippines. Any dispute arising out of or related
+              to these terms and conditions shall be resolved exclusively in the
+              courts of the Philippines.
+            </li>
+          </ul>
 
-            <h6>
-            By using our website, you acknowledge that you have read, understood, and agree to be bound by 
-            these terms and conditions. If you do not agree to these terms and conditions, please do not use 
+          <h6>
+            By using our website, you acknowledge that you have read,
+            understood, and agree to be bound by these terms and conditions. If
+            you do not agree to these terms and conditions, please do not use
             our website.
-            </h6>
-
+          </h6>
         </div>
       </div>
-      
-
-  </section>
+    </section>
+  )
 }
--- src/pages/terms-and-condition.tsx
+++ src/pages/terms-and-condition.tsx
@@ -1,76 +1,102 @@
 import Image from "next/image"
 import Link from "next/link"
 
- export default function HeaderSection() {
-    return <section className="terms-and-condition-header">
+export default function HeaderSection() {
+  return (
+    <section className="terms-and-condition-header">
       <div className="termsandcondition">
-          <div className="header">
+        <div className="[background-color:_#10b981] h-72 flex items-center">
+          <div className="max-w-5xl mx-auto grid grid-cols-[1fr_auto] items-center w-full px-6">
             <div>
-              <h4> Legal Terms </h4>
-              <h1> Filipino de Cuisine Terms of Services </h1>
-              <p>Last Update: March 25, 2023</p>
-              </div>
+              <h4 className="text-white font-semibold text-xl mb-2">
+                Legal Terms{" "}
+              </h4>
+              <h1 className="text-white font-bold text-3xl mb-2">
+                Filipino de Cuisine Terms of Services
+              </h1>
+              <p className="text-xl text-white font-medium">
+                Last updated: March 25, 2023
+              </p>
+            </div>
 
             <div className="image">
-            
-               <Link href={"/privacy"}>
-               <Image 
-             src="/assets/terms-and-condition/symbol1.png"
-             alt="next"
-             width={40}
-             height={40}
-            
-               />
-               </Link>
-      </div>
-          </div>
-
-          <div className="description">
-              <p>
-                1.  Information We Collect: We may collect personal information such as your name, email address, 
-              phone number, and payment information when you make a reservation or order food through our 
-              website. We also collect non-personal information such as your IP address and browser type when 
-              you visit our website.
-                <br></br> <br></br>
-                2.  How We Use Your Information: We use your personal information to process your reservations and 
-              orders, communicate with you about your reservations and orders, and improve our services. We 
-              may also use your personal information to send you marketing communications, but only if you 
-              have given us your consent to do so.
-                <br></br> <br></br>
-                3.  How We Disclose Your Information: We do not sell, rent, or disclose your personal 
-                information to third parties without your consent, except as required by law. We may 
-                disclose your personal information to our service providers who assist us in processing 
-                your reservations and orders.
-                <br></br> <br></br>
-                4.  Cookies: We use cookies to improve your experience on our website: Cookies are small text 
-                files that are stored on your device when you visit our website. We use both session cookies, 
-                which expire when you close your browser, and persistent cookies, which remain on your device 
-                until they expire or you delete them.
-                <br></br> <br></br>
-                5.  Security: We take reasonable measures to protect your personal information from unauthorized
-                 access, use, or disclosure. However, no method of transmission over the Internet or electronic
-                  storage is 100% secure, so we cannot guarantee absolute security.
-                  <br></br> <br></br>
-                6.  Third-Party Websites: Our website may contain links to third-party websites that are not 
-                owned or controlled by Filipino de Cuisine Restaurant. We are not responsible for the content 
-                or privacy practices of these websites. You acknowledge and agree that we shall not be liable 
-                for any damages or losses arising from your use of these third-party websites.
-                <br></br> <br></br>
-                7. Children&apos;s Privacy: Our website is not intended for children under the age of 13, and we do 
-                 not knowingly collect personal information from children under the age of 13. If we learn that
-                 we have collected personal information from a child under the age of 13, we will promptly 
-                 delete that information.
-                 <br></br> <br></br>
-                8. Changes to Privacy Policy: We reserve the right to modify or update this privacy policy at
-                 any time without prior notice. Your continued use of our website after any such changes 
-                 signifies your acceptance of the updated policy.
-              </p>
-
+              <Link href={"/privacy"}>
+                <Image
+                  src="/assets/terms-and-condition/symbol1.png"
+                  alt="next"
+                  width={40}
+                  height={40}
+                />
+              </Link>
+            </div>
           </div>
         </div>
-        
 
+        <ol className="description list-decimal list-outside text-justify mx-auto max-w-4xl my-20 pl-12 pr-6">
+          <li className="mb-6">
+            <span className="font-medium">Information We Collect</span>: We may
+            collect personal information such as your name, email address, phone
+            number, and payment information when you make a reservation or order
+            food through our website. We also collect non-personal information
+            such as your IP address and browser type when you visit our website.
+          </li>
+          <li className="mb-6">
+            <span className="font-medium">How We Use Your Information</span>: We
+            use your personal information to process your reservations and
+            orders, communicate with you about your reservations and orders, and
+            improve our services. We may also use your personal information to
+            send you marketing communications, but only if you have given us
+            your consent to do so.
+          </li>
+          <li className="mb-6">
+            <span className="font-medium">
+              How We Disclose Your Information
+            </span>
+            : We do not sell, rent, or disclose your personal information to
+            third parties without your consent, except as required by law. We
+            may disclose your personal information to our service providers who
+            assist us in processing your reservations and orders.
+          </li>
+          <li className="mb-6">
+            <span className="font-medium">Cookies</span>: We use cookies to
+            improve your experience on our website: Cookies are small text files
+            that are stored on your device when you visit our website. We use
+            both session cookies, which expire when you close your browser, and
+            persistent cookies, which remain on your device until they expire or
+            you delete them.
+          </li>
+          <li className="mb-6">
+            <span className="font-medium">Security</span>: We take reasonable
+            measures to protect your personal information from unauthorized
+            access, use, or disclosure. However, no method of transmission over
+            the Internet or electronic storage is 100% secure, so we cannot
+            guarantee absolute security.
+          </li>
+          <li className="mb-6">
+            <span className="font-medium">Third-Party Websites</span>: Our
+            website may contain links to third-party websites that are not owned
+            or controlled by Filipino de Cuisine Restaurant. We are not
+            responsible for the content or privacy practices of these websites.
+            You acknowledge and agree that we shall not be liable for any
+            damages or losses arising from your use of these third-party
+            websites.
+          </li>
+          <li className="mb-6">
+            <span className="font-medium">Children&apos;s Privacy</span>: Our
+            website is not intended for children under the age of 13, and we do
+            not knowingly collect personal information from children under the
+            age of 13. If we learn that we have collected personal information
+            from a child under the age of 13, we will promptly delete that
+            information.
+          </li>
+          <li>
+            <span className="font-medium">Changes to Privacy Policy</span>: We
+            reserve the right to modify or update this privacy policy at any
+            time without prior notice. Your continued use of our website after
+            any such changes signifies your acceptance of the updated policy.
+          </li>
+        </ol>
+      </div>
     </section>
-  }
-
-  
+  )
+}
