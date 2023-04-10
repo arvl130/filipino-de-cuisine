0af6c98f15a90f7501065d683bd86fc3afcced9f
Angelo Geulin <angelogeulin123@gmail.com>
2023-04-10 13:51:44 +0800

    refactor: extract getBaseUrl into its own file

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,23 +4,7 @@ import type { RootRouter } from "../server/trpc/routers/root"
 import { getIdToken } from "firebase/auth"
 import { auth } from "./auth"
 import SuperJSON from "superjson"
-
-function getBaseUrl() {
-  if (typeof window !== "undefined")
-    // browser should use relative path
-    return ""
-
-  if (process.env.VERCEL_URL)
-    // reference for vercel.com
-    return `https://${process.env.VERCEL_URL}`
-
-  if (process.env.RENDER_INTERNAL_HOSTNAME)
-    // reference for render.com
-    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`
-
-  // assume localhost
-  return `http://localhost:${process.env.PORT ?? 3000}`
-}
+import { getBaseUrl } from "./base-url"
 
 export const api = createTRPCNext<RootRouter>({
   config({ ctx }) {
