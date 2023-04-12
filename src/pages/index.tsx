3a11d94fe355f36eea1400f2c5cbed478a1c783d
Angelo Geulin <angelogeulin123@gmail.com>
2023-04-12 20:07:54 +0800

    chore: remove hardcoded basket svg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Basket } from "@/components/HeroIcons"
 import { MenuItem as FeaturedDishesSectionItem } from "@/components/menu/MenuItem"
 import { Prisma } from "@prisma/client"
 import Head from "next/head"
@@ -132,20 +133,9 @@ function FeaturedDishesSection() {
     <section className="[border-color:_#F5F5F5] border-2 pt-8 pb-12 px-6">
       <div className="grid grid-cols-[4rem_1fr_4rem]">
         <div className="flex justify-end">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-10 h-10 [color:_#10B981]"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
-            />
-          </svg>
+          <Link href="/menu/basket" className="text-emerald-500">
+            <Basket />
+          </Link>
         </div>
         <div className="text-center">
           <h2 className="[color:_#78716C] mb-3">Customers Pick</h2>
--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,3 +1,4 @@
+import { Basket } from "@/components/HeroIcons"
 import { MenuItem as MenuPageSectionItem } from "@/components/menu/MenuItem"
 import { api } from "@/utils/api"
 import { MenuItem } from "@prisma/client"
@@ -14,21 +15,8 @@ function MenuPageSections({ menuItems }: { menuItems: MenuItem[] }) {
       <div className="grid sm:grid-cols-[4rem_1fr_4rem] mb-12">
         {/* Basket */}
         <div className="flex justify-end sm:jusify-start mb-3 sm:mb-0">
-          <Link href="/menu/basket">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-10 h-10 [color:_#10B981]"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
-              />
-            </svg>
+          <Link href="/menu/basket" className="text-emerald-500">
+            <Basket />
           </Link>
         </div>
         {/* Tabs */}
