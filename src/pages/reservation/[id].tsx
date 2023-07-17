7a6d0ef2638ddbafebe8cb69fe61758557ea81c6
Angelo Geulin <angelogeulin123@gmail.com>
2023-07-17 19:50:39 +0800

    style: keep padding bottom on mobile

--- src/pages/reservation/[id].tsx
+++ src/pages/reservation/[id].tsx
@@ -418,7 +418,7 @@ function AuthenticatedPage({ user }: { user: User }) {
 
 export default function ViewReservationPage() {
   return (
-    <main className="max-w-7xl mx-auto w-full px-3 sm:px-6 py-4 sm:py-12">
+    <main className="max-w-7xl mx-auto w-full px-3 sm:px-6 py-4 sm:pt-12 pb-12">
       <ProtectedPage>
         {({ user }) => <AuthenticatedPage user={user} />}
       </ProtectedPage>
